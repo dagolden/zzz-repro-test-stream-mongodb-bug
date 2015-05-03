937c4a59da7cce216fe2eb96ec264e684c91f07b
David Golden <dagolden@cpan.org>
2015-05-03 08:30:36 -0400

    set grep test to true; still repros

--- test.pl
+++ test.pl
@@ -21,7 +21,7 @@ $coll->ensure_index({"x.y" => 1}, {"name" => "foo"});
 
 ok(1);
 Dump($_);
-my ($index) = grep { $_->{name} eq 'foo' } $coll->get_indexes;
+my ($index) = grep { 1 } $coll->get_indexes;
 Dump($_);
 Dump($index);
 ok(1);
